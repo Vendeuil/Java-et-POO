5163e2a393d2042e6178659bfcc00c05d98f6db1
Vendeuil <108340085+Vendeuil@users.noreply.github.com>
2022-07-13 15:59:45 +0200

    Update SalarieDao.java

--- Java/Resto/src/main/java/dao/SalarieDao.java
+++ Java/Resto/src/main/java/dao/SalarieDao.java
@@ -1,5 +1,16 @@
-package dao;
+package DAO;
 
-public class SalarieDao {
 
+import Models.*;
+
+public interface SalarieDAO {
+	//DEBUT des 4 méthodes CRUD
+		void create(int id, String nom, String prenom, String code_restaurant);
+		void read(int id);
+		void update(int id);
+		void delete(int id);
+	//FIN
+		int nbrHomonymes(String nom, String prenom);
+		int id(String nom, String prenom);
+		boolean travailleIci(Salarie sal, Restaurant res);
 }
