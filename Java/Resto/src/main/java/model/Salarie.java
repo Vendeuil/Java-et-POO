ec0a5c689556b765ea2e50ec2e53cc05b1a0401d
Vendeuil <108340085+Vendeuil@users.noreply.github.com>
2022-07-13 15:57:49 +0200

    Update Salarie.java

--- Java/Resto/src/main/java/model/Salarie.java
+++ Java/Resto/src/main/java/model/Salarie.java
@@ -1,5 +1,15 @@
-package model;
+package Models;
 
 public class Salarie {
-
+	int id;//pk
+	String nom;
+	String prenom;
+	double salaire;
+	String code_restaurant;//fk
+	public Salarie(int id, String nom, String prenom, String code_restaurant) {
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.code_restaurant = code_restaurant;
+	}
 }
