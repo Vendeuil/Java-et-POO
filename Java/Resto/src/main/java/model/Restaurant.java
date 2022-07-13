6a0f3587d2c2743cd6736fc5ed4defdf0508faa3
Vendeuil <108340085+Vendeuil@users.noreply.github.com>
2022-07-13 15:53:52 +0200

    Update Restaurant.java

--- Java/Resto/src/main/java/model/Restaurant.java
+++ Java/Resto/src/main/java/model/Restaurant.java
@@ -1,5 +1,34 @@
-package model;
+package Models;
 
 public class Restaurant {
-
+	String code;//pk
+	String nom;
+	String adresse;
+	int nbr_table;
+	boolean statut;
+	Salarie[] staff;
+	Menu[] menus;
+	public Restaurant(String code, String nom, String adresse) {
+		this.code = code;
+		this.nom = nom;
+		this.adresse = adresse;
+	}
+	public String getCode() {
+		return code;
+	}
+//	public void setCode(String code) {
+//		this.code = code;
+//	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
 }
