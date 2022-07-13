0ad0087163f8b8b3257ed9e0e38f2073a385f73c
Vendeuil <108340085+Vendeuil@users.noreply.github.com>
2022-07-13 15:58:55 +0200

    Update RestaurantDao.java

--- Java/Resto/src/main/java/dao/RestaurantDao.java
+++ Java/Resto/src/main/java/dao/RestaurantDao.java
@@ -1,5 +1,12 @@
-package dao;
+package DAO;
 
-public class RestaurantDao {
-
+public interface RestaurantDAO {
+//DEBUT des 4 méthodes CRUD
+	void create(String code, String nom, String adresse);
+	void read(String code);//Via la pk
+	void update(String code);//Via la pk
+	void delete(String code);//Via la pk
+// FIN
+	boolean existeCeCode(String code);
+	boolean existeCeNom(String nom);
 }
